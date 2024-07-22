0dd8d295262cfb53d0fb214c9fa959c0bb92b577
daniel <4954577+jaensen@users.noreply.github.com>
2024-07-22 04:08:28 +0200

    remove service worker registration

--- circles-app/src/routes/+layout.svelte
+++ circles-app/src/routes/+layout.svelte
@@ -9,15 +9,15 @@
     import UpdateBanner from "$lib/components/UpdateBanner.svelte";
     import {page} from "$app/stores";
 
-    if ('serviceWorker' in navigator) {
-        window.addEventListener('load', () => {
-            navigator.serviceWorker.register('/service-worker.js').then(registration => {
-                console.log('ServiceWorker registration successful with scope: ', registration.scope);
-            }, err => {
-                console.log('ServiceWorker registration failed: ', err);
-            });
-        });
-    }
+    // if ('serviceWorker' in navigator) {
+    //     window.addEventListener('load', () => {
+    //         navigator.serviceWorker.register('/service-worker.js').then(registration => {
+    //             console.log('ServiceWorker registration successful with scope: ', registration.scope);
+    //         }, err => {
+    //             console.log('ServiceWorker registration failed: ', err);
+    //         });
+    //     });
+    // }
 </script>
 
 {#if $avatar}
