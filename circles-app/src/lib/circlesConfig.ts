ba705fcace0a0d6024e3418423e5319091c23f5a
daniel <4954577+jaensen@users.noreply.github.com>
2024-07-22 05:36:03 +0200

    config

--- circles-app/src/lib/circlesConfig.ts
+++ circles-app/src/lib/circlesConfig.ts
@@ -1,14 +1,14 @@
 import type {CirclesConfig} from "@circles-sdk/sdk";
 
 export const circlesConfig: CirclesConfig = {
-    // circlesRpcUrl: "https://chiado-rpc.aboutcircles.com",
-    // pathfinderUrl: "https://chiado-pathfinder.aboutcircles.com",
-    // v2PathfinderUrl: "https://chiado-pathfinder.aboutcircles.com/pathfinder/",
-    // profileServiceUrl: "https://chiado-pathfinder.aboutcircles.com/profiles/",
-    circlesRpcUrl: "http://localhost:8545",
-    pathfinderUrl: "http://localhost:8080",
-    v2PathfinderUrl: "http://localhost:8080",
-    profileServiceUrl: "http://localhost:3000",
+    circlesRpcUrl: "https://chiado-rpc.aboutcircles.com",
+    pathfinderUrl: "https://chiado-pathfinder.aboutcircles.com",
+    v2PathfinderUrl: "https://chiado-pathfinder.aboutcircles.com/pathfinder/",
+    profileServiceUrl: "https://chiado-pathfinder.aboutcircles.com/profiles/",
+    // circlesRpcUrl: "http://localhost:8545",
+    // pathfinderUrl: "http://localhost:8080",
+    // v2PathfinderUrl: "http://localhost:8080",
+    // profileServiceUrl: "http://localhost:3000",
     v1HubAddress: "0xdbf22d4e8962db3b2f1d9ff55be728a887e47710",
     v2HubAddress: "0x2066CDA98F98397185483aaB26A89445addD6740",
     migrationAddress: "0x2A545B54bb456A0189EbC53ed7090BfFc4a6Af94",
