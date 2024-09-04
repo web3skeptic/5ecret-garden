6fb72a39cf691533948f1db3ac734df7890fba57
daniel <4954577+jaensen@users.noreply.github.com>
2024-09-04 20:15:55 +0200

    add a separate /wrap page to wrap token to ERC20

---
+++ circles-app/src/routes/wrap/+page.svelte
@@ -0,0 +1,70 @@
+<script lang="ts">
+    import {ethers} from "ethers6";
+    import {avatar} from "$lib/stores/avatar";
+    import {circles} from "$lib/stores/circles";
+    import AssetPicker from "$lib/components/AssetPicker.svelte";
+    import type {TokenBalanceRow} from "@circles-sdk/data";
+    import {onMount} from "svelte";
+    import ActionButton from "$lib/components/ActionButton.svelte";
+
+    let valueString: string = "";
+    let selectedCollateral: string | undefined;
+    let balances: TokenBalanceRow[] = [];
+
+    onMount(async () => {
+        if ($avatar?.address && $circles) {
+            balances = await $circles.data.getTokenBalancesV2($avatar?.address);
+        }
+        selectedCollateral = balances.find(b => b.tokenOwner === $avatar?.address)?.tokenOwner;
+    });
+
+    async function wrapInflationary() {
+        if ($avatar?.avatarInfo?.version !== 2) {
+            throw new Error("Only supported for Avatar v2");
+        }
+        if (!selectedCollateral) {
+            return;
+        }
+        const sendValue = ethers.parseEther(valueString.toString());
+        const receipt = await $avatar?.wrapInflationErc20(selectedCollateral, sendValue);
+        console.log("Wrap receipt:", receipt);
+    }
+
+    async function wrapDemurraged() {
+        if ($avatar?.avatarInfo?.version !== 2) {
+            throw new Error("Only supported for Avatar v2");
+        }
+        if (!selectedCollateral) {
+            return;
+        }
+        const sendValue = ethers.parseEther(valueString.toString());
+        const receipt = await $avatar?.wrapDemurrageErc20(selectedCollateral, sendValue);
+        console.log("Wrap receipt:", receipt);
+    }
+</script>
+
+<div class="mt-4">
+    <label for="asset" class="block text-sm font-medium text-gray-700">Asset</label>
+    <div id="asset">
+        <AssetPicker {balances} bind:selectedCollateral/>
+    </div>
+</div>
+
+<div class="mt-4">
+    <label for="amount" class="block text-sm font-medium text-gray-700">Amount</label>
+    <div class="flex">
+        <input bind:value={valueString} type="number" id="amount"
+               class="mt-1 block w-full p-2 border border-gray-300 rounded-l-md"
+               placeholder="0.00">
+    </div>
+</div>
+
+<!-- Send Button -->
+<div class="mt-4">
+    <ActionButton action={wrapInflationary} disabled={!selectedCollateral}>
+        Wrap as inflation ERC20
+    </ActionButton>
+    <ActionButton action={wrapDemurraged} disabled={!selectedCollateral}>
+        Wrap as demurrage ERC20
+    </ActionButton>
+</div>
