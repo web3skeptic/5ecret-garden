5fc899293e73eccaa04c50dc176e9c7ddd2bd8b9
daniel <4954577+jaensen@users.noreply.github.com>
2024-09-17 04:50:10 +0200

    wire up trust

--- circles-app/src/lib/flows/Trust.svelte
+++ circles-app/src/lib/flows/Trust.svelte
@@ -1,1 +1,40 @@
-Trust
+<script lang="ts">
+    import Avatar from "$lib/components/Avatar.svelte";
+    import {avatar} from "$lib/stores/avatar";
+
+    export let address: string;
+
+    async function trust() {
+        if (!$avatar) {
+            throw new Error("Avatar store not available");
+        }
+        await $avatar?.trust(address);
+    }
+</script>
+
+<div class="p-6 pt-0">
+    <p class="mb-4">
+        You're about to trust the following group or person.
+    </p>
+    <Avatar address={address}>
+        {address}
+    </Avatar>
+    <div role="alert" class="mt-4 alert alert-warning">
+        <svg
+                xmlns="http://www.w3.org/2000/svg"
+                class="h-6 w-6 shrink-0 stroke-current"
+                fill="none"
+                viewBox="0 0 24 24">
+            <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    stroke-width="2"
+                    d="M12 9v2m0 4h.01m-6.938 4h13.856c1.54 0 2.502-1.667 1.732-3L13.732 4c-.77-1.333-2.694-1.333-3.464 0L3.34 16c-.77 1.333.192 3 1.732 3z" />
+        </svg>
+        <span>Trusting someone means that you accept their tokens at the same value as your own.</span>
+    </div>
+
+    <div class="modal-action">
+        <button type="submit" class="btn btn-primary" on:click={async () => await trust()}>Trust</button>
+    </div>
+</div>
