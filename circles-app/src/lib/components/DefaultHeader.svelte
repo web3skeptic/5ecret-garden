c3645dadd69c10899c9170b72d5fed15cb59f476
Wagalidoom <lukas-8@live.fr>
2024-10-11 22:44:09 +0200

    fix: default header warnings

--- circles-app/src/lib/components/DefaultHeader.svelte
+++ circles-app/src/lib/components/DefaultHeader.svelte
@@ -19,7 +19,7 @@
     }[] = [];
 </script>
 
-<div class="navbar bg-base-100 font-dmSans">
+<div class="navbar font-dmSans">
     <div class="navbar-start">
         <div class="dropdown">
             <div tabindex="0" role="button" class="btn btn-ghost lg:hidden">
@@ -28,12 +28,12 @@
                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h8m-8 6h16"/>
                 </svg>
             </div>
-            <ul tabindex="0"
+            <ul
                 class="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow">
                 {#each menuItems as item}
-                    <!-- <li>
-                        <a href={item.link}>{item.name}</a>
-                    </li> -->
+                    <li>
+                        <a tabindex="0" href={item.link}>{item.name}</a>
+                    </li>
                 {/each}
             </ul>
         </div>
@@ -61,7 +61,7 @@
                 }
             }}>
                 {#if action.icon}
-                    <img class="h-6 w-6" src={action.icon}/>
+                    <img class="h-6 w-6" src={action.icon} alt={action.name}/>
                 {/if}
                 {action.name}
             </a>
