6e4b7ecf36fa0663fa3ca7526e4d95e7783b0a2a
Wagalidoom <lukas-8@live.fr>
2024-10-14 14:30:50 +0200

    feat: update contact profile

--- circles-app/src/lib/pages/Untrust.svelte
+++ circles-app/src/lib/pages/Untrust.svelte
@@ -32,26 +32,28 @@
   }
 </script>
 
-<div class="p-6 pt-0 mt-4">
+<div class="flex flex-col gap-y-4 mt-8">
   <p class="mb-4">You're about to un-trust the following group or person:</p>
   <Avatar {address} clickable={false}>
     {address}
   </Avatar>
-  <div role="alert" class="mt-4 alert alert-warning">
+  <div role="alert" class="flex gap-x-2 bg-[#FDE8E8] p-2 rounded-lg">
     <svg
-      xmlns="http://www.w3.org/2000/svg"
-      class="h-6 w-6 shrink-0 stroke-current"
+      class="h-6 w-6"
+      viewBox="0 0 18 19"
       fill="none"
-      viewBox="0 0 24 24"
+      xmlns="http://www.w3.org/2000/svg"
     >
       <path
-        stroke-linecap="round"
-        stroke-linejoin="round"
-        stroke-width="2"
-        d="M12 9v2m0 4h.01m-6.938 4h13.856c1.54 0 2.502-1.667 1.732-3L13.732 4c-.77-1.333-2.694-1.333-3.464 0L3.34 16c-.77 1.333.192 3 1.732 3z"
+        fill-rule="evenodd"
+        clip-rule="evenodd"
+        d="M7.43165 3.28909C8.12015 2.06509 9.88145 2.06509 10.5691 3.28909L15.5911 12.2171C16.2661 13.4177 15.3994 14.8991 14.0233 14.8991H3.97835C2.60135 14.8991 1.73465 13.4177 2.40965 12.2171L7.43165 3.28909ZM9.90035 12.2C9.90035 12.4387 9.80553 12.6676 9.63675 12.8364C9.46797 13.0052 9.23905 13.1 9.00035 13.1C8.76166 13.1 8.53274 13.0052 8.36396 12.8364C8.19517 12.6676 8.10035 12.4387 8.10035 12.2C8.10035 11.9613 8.19517 11.7324 8.36396 11.5636C8.53274 11.3948 8.76166 11.3 9.00035 11.3C9.23905 11.3 9.46797 11.3948 9.63675 11.5636C9.80553 11.7324 9.90035 11.9613 9.90035 12.2ZM9.00035 4.99999C8.76166 4.99999 8.53274 5.09482 8.36396 5.2636C8.19517 5.43238 8.10035 5.6613 8.10035 5.89999V8.59999C8.10035 8.83869 8.19517 9.06761 8.36396 9.23639C8.53274 9.40517 8.76166 9.49999 9.00035 9.49999C9.23905 9.49999 9.46797 9.40517 9.63675 9.23639C9.80553 9.06761 9.90035 8.83869 9.90035 8.59999V5.89999C9.90035 5.6613 9.80553 5.43238 9.63675 5.2636C9.46797 5.09482 9.23905 4.99999 9.00035 4.99999Z"
+        fill="#DF7B6B"
       />
     </svg>
-    <span>Un-trusting means that you no longer accept their tokens.</span>
+    <span class="text-[#DF7B6B] font-semibold"
+      >Un-trusting means that you no longer accept their tokens.</span
+    >
   </div>
 
   <div>
