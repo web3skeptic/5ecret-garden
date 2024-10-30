880db799834aa726d86d9cb5cf666c0b04de3562
Wagalidoom <lukas-8@live.fr>
2024-10-30 23:09:29 +0100

    feat: new design for dashboard page (wip)

--- circles-app/src/lib/components/TotalBalance.svelte
+++ circles-app/src/lib/components/TotalBalance.svelte
@@ -1,15 +1,16 @@
 <script>
   import { circlesBalances } from '$lib/stores/circlesBalances';
   import { totalCirclesBalance } from '$lib/stores/totalCirclesBalance';
-  import {roundToDecimals} from "$lib/utils/shared";
+  import { roundToDecimals } from '$lib/utils/shared';
 </script>
 
-<div class="flex flex-col items-center gap-y-4 w-full border rounded-lg py-4">
-  <p class="bg-gray-200 py-1 px-4 rounded-full text-sm">Balance</p>
+<div class="flex flex-col items-center gap-y-4 w-full mt-10">
+  <p class="text-sm text-black/50">Your Balance</p>
   <div class="stat-value">
     {roundToDecimals($totalCirclesBalance)} Circles
   </div>
-  <a href="/_new/dashboard/balances" class="stat-desc text-black underline">
-    {$circlesBalances?.data?.length} individual tokens
-  </a>
+  <p class="text-sm">
+    {$circlesBalances?.data?.length} individual tokens •
+    <a href="/_new/dashboard/balances" class="underline"> See breakdown </a>
+  </p>
 </div>
