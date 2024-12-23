07502b32760affb494630547101b15ce3b22bee3
ilge.ustun <153606956+ilge-ustun@users.noreply.github.com>
2024-12-23 17:17:07 +0100

    refactor Avatar

--- circles-app/src/routes/dashboard/TransactionRow.svelte
+++ circles-app/src/routes/dashboard/TransactionRow.svelte
@@ -1,21 +1,22 @@
 <script lang="ts">
-  import Avatar from '$lib/components/Avatar.svelte';
+  import Avatar from '$lib/components/Avatar/Avatar.svelte';
   import { crcTypes, staticTypes } from '$lib/utils/shared';
   import { roundToDecimals } from '$lib/utils/shared';
   import type { TransactionHistoryRow } from '@circles-sdk/data';
   import { tokenTypeToString } from '$lib/pages/SelectAsset.svelte';
   import { avatar } from '$lib/stores/avatar';
+  import { getTimeAgo } from '$lib/utils/shared';
 
   export let item: TransactionHistoryRow;
 
   const getBadge = () => {
     if (!$avatar) return;
     if (item.from === '0x0000000000000000000000000000000000000000')
-      return '/circles-badge.svg';
+      return '/badge-mint.svg';
     if (item.to === '0x0000000000000000000000000000000000000000')
-      return '/logo.svg';
-    if (item.from === $avatar.address) return '/sent-badge.svg';
-    if (item.to === $avatar.address) return '/received-badge.svg';
+      return '/badge-burn.svg';
+    if (item.from === $avatar.address) return '/badge-sent.svg';
+    if (item.to === $avatar.address) return '/badge-received.svg';
   };
 
   const getTransactionText = () => {
@@ -42,7 +43,7 @@
 <div
   class="flex items-center justify-between p-2 bg-base-100 hover:bg-base-200 rounded-lg"
 >
-  <Avatar
+  <!-- <Avatar
     address={counterpartyAddress}
     view="transaction_row"
     pictureOverlayUrl={getBadge()}
@@ -55,7 +56,22 @@
     >
       {getTransactionText()}
     </a>
-  </Avatar>
+  </Avatar> -->
+  <Avatar
+    address={counterpartyAddress}
+    view="horizontal"
+    pictureOverlayUrl={getBadge()}
+    topInfo={getTransactionText()}
+    bottomInfo={getTimeAgo(item.timestamp)}
+  />
+    <!-- <a
+      target="_blank"
+      href={'https://gnosisscan.io/tx/' + item.transactionHash}
+      class="text-xs md:text-sm text-gray-500"
+    >
+      {getTransactionText()}
+    </a>
+  </Avatar> -->
 
   <div class="col text-right">
     {#if item.from === $avatar.address}
