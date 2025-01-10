aa3b00e9824286278699c288b9ab6ff2e0df020f
Wagalidoom <lukas-8@live.fr>
2025-01-10 20:47:05 +0100

    style: improve style of register components

--- circles-app/src/routes/register/register-v1-person/+page.svelte
+++ circles-app/src/routes/register/register-v1-person/+page.svelte
@@ -1,38 +1,33 @@
 <script lang="ts">
-    import {goto} from "$app/navigation";
-    import ActionButton from "$lib/components/ActionButton.svelte";
-    import {avatar} from "$lib/stores/avatar";
-    import {circles} from "$lib/stores/circles";
-    import type {Avatar} from "@circles-sdk/sdk";
+  import { goto } from '$app/navigation';
+  import ActionButton from '$lib/components/ActionButton.svelte';
+  import { avatar } from '$lib/stores/avatar';
+  import { circles } from '$lib/stores/circles';
+  import type { Avatar } from '@circles-sdk/sdk';
 
-    async function registerHuman() {
-        if (!$circles) {
-            throw new Error('Wallet not connected ($circles is undefined)');
-        }
-
-        $avatar = <Avatar>await $circles.registerHuman();
-
-        await goto("/dashboard");
+  async function registerHuman() {
+    if (!$circles) {
+      throw new Error('Wallet not connected ($circles is undefined)');
     }
+
+    $avatar = <Avatar>await $circles.registerHuman();
+
+    await goto('/dashboard');
+  }
 </script>
 
-<div class="hero">
-    <div class="hero-content flex-col lg:flex-row-reverse">
-
-        <div class="card bg-base-100 w-96 shadow-xl">
-            <figure class="px-10 pt-10">
-                <img src="/person.svg"
-                     alt="Shoes"
-                     class="w-16 h-16 rounded-xl"/>
-            </figure>
-            <div class="card-body items-center text-center">
-                <h2 class="card-title">Register person</h2>
-                <div class="card-actions">
-                    <ActionButton action={registerHuman}>
-                        Create
-                    </ActionButton>
-                </div>
-            </div>
-        </div>
+<div
+  class="w-full flex flex-col min-h-screen p-4 max-w-xl gap-y-4 justify-center"
+>
+  <div class="card bg-base-100 w-96 border shadow-sm">
+    <figure class="px-10 pt-10">
+      <img src="/person.svg" alt="Shoes" class="w-16 h-16 rounded-xl" />
+    </figure>
+    <div class="card-body items-center text-center">
+      <h2 class="card-title">Register person</h2>
+      <div class="card-actions">
+        <ActionButton action={registerHuman}>Create</ActionButton>
+      </div>
     </div>
+  </div>
 </div>
