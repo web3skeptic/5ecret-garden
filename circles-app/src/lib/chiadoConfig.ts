f2eaaf7ab545315f8ada14747e3557fc8fb952a3
daniel <4954577+jaensen@users.noreply.github.com>
2025-02-13 20:37:59 +0100

    chore: switch to rpc.aboutcircles.com

--- circles-app/src/lib/chiadoConfig.ts
+++ circles-app/src/lib/chiadoConfig.ts
@@ -16,12 +16,12 @@ export const chiadoConfig: CirclesConfig = {
 };
 
 export const gnosisConfig: CirclesConfig = {
-    circlesRpcUrl: "https://rpc.circlesubi.network/",
+    circlesRpcUrl: "https://rpc.aboutcircles.com/",
     pathfinderUrl: "https://pathfinder.aboutcircles.com",
     v1HubAddress: "0x29b9a7fbb8995b2423a71cc17cf9810798f6c543",
     v2HubAddress: "0xc12C1E50ABB450d6205Ea2C3Fa861b3B834d13e8",
     nameRegistryAddress: "0xA27566fD89162cC3D40Cb59c87AAaA49B85F3474",
     migrationAddress: "0xD44B8dcFBaDfC78EA64c55B705BFc68199B56376",
-    profileServiceUrl: "https://rpc.circlesubi.network/profiles",
+    profileServiceUrl: "https://rpc.aboutcircles.com/profiles",
     baseGroupMintPolicy: "0xcCa27c26CF7BAC2a9928f42201d48220F0e3a549",
 }
