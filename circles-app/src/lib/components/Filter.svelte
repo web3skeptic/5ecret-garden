ff17093bda102952f3977810cdf27e7270e678cd
Wagalidoom <lukas-8@live.fr>
2025-04-03 18:10:29 +0200

    feat: add filter to balance breakdown

---
+++ circles-app/src/lib/components/Filter.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+  import type { Writable } from 'svelte/store';
+
+  interface Props {
+    text: string;
+    filter: Writable<any | undefined>;
+    value: any;
+  }
+
+  let { text, filter, value }: Props = $props();
+</script>
+
+<button
+  class={`bg-[#F3F4F6] border-none rounded-lg px-2 py-1 text-sm flex flex-row items-center gap-x-1 font-medium hover:text-black/70 hover:cursor-pointer ${
+    $filter === value ? 'text-black' : 'text-gray-400'
+  }`}
+  onclick={() => filter.set(value)}
+>
+  {text}
+</button>
