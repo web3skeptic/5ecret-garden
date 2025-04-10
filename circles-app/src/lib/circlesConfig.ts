6735b324b16ff5becdf5635dc9d6588000605a3c
jaensen <4954577+jaensen@users.noreply.github.com>
2025-04-10 03:44:22 +0200

    fix: config

--- circles-app/src/lib/circlesConfig.ts
+++ circles-app/src/lib/circlesConfig.ts
@@ -12,9 +12,9 @@ export const chiadoConfig: CirclesConfig = {
 };
 
 export const gnosisConfig: CirclesConfig = {
-  circlesRpcUrl: 'http://localhost:8545/',
+  circlesRpcUrl: 'https://rpc.aboutcircles.com/',
   pathfinderUrl: 'https://pathfinder.aboutcircles.com',
-  profileServiceUrl: 'http://localhost:3000',
+  profileServiceUrl: 'https://rpc.aboutcircles.com/profiles/',
   v1HubAddress: '0x29b9a7fbb8995b2423a71cc17cf9810798f6c543',
   v2HubAddress: '0xc12C1E50ABB450d6205Ea2C3Fa861b3B834d13e8',
   nameRegistryAddress: '0xA27566fD89162cC3D40Cb59c87AAaA49B85F3474',
