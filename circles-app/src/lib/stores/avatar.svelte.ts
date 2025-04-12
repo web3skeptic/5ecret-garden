ddc60a0343c6a5d300595699bda9df73b1abbf64
Wagalidoom <lukas-8@live.fr>
2025-04-12 22:40:25 +0200

    fix: remove temp avatar store

--- circles-app/src/lib/stores/avatar.svelte.ts
+++ circles-app/src/lib/stores/avatar.svelte.ts
@@ -1,13 +1,4 @@
 import type { Profile } from '@circles-sdk/profiles';
 import type { Avatar } from '@circles-sdk/sdk';
-import { readable } from 'svelte/store';
 
 export let avatarState: { avatar: Avatar | undefined, isGroup: boolean | undefined, profile: Profile | undefined } = $state({ avatar: undefined, isGroup: undefined, profile: undefined });
-
-export const avatarStore = readable(avatarState.avatar, (set) => {
-    $effect(() => {
-        if (avatarState.avatar) {
-            set(avatarState.avatar);
-        }
-    });
-});
