3099d886be5f94a0678a725be4513aeae027c408
Wagalidoom <lukas-8@live.fr>
2025-05-21 09:51:51 +0200

    feat: add terms and privacy policy content

--- circles-app/src/routes/terms/+page.svelte
+++ circles-app/src/routes/terms/+page.svelte
@@ -1,1 +1,7 @@
-<div class="mt-28 font-bold">TERMS OF USE</div>
+<script lang="ts">
+  import Terms from './terms.svx';
+</script>
+
+<div class="mt-28 w-full flex flex-col items-center justify-center">
+  <div class="max-w-xl flex flex-col items-center justify-center gap-y-4"><Terms /></div>
+</div>
